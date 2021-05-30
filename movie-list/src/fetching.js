e5fe1c13864b5fa9ce1b6fde63a455b94302f504
StyledDEV <apbdev.contact@gmail.com>
2021-05-30 19:23:12 -0300

    bugfixes

--- movie-list/src/fetching.js
+++ movie-list/src/fetching.js
@@ -1,26 +1,24 @@
 /* GET YOUR API AT http://www.omdbapi.com/apikey.aspx */
-const API_KEY = '275405d5'
+const API_KEY = "275405d5";
 
+const upperFirstLetter = (string) =>
+	string.charAt(0).toUpperCase() + string.slice(1);
 
-
-
-const upperFirstLetter = string => string.charAt(0).toUpperCase() + string.slice(1)
-
-
-const defaultMoviePoster = 'https://artgalleryofballarat.com.au/wp-content/uploads/2020/06/placeholder-image.png'
+const defaultMoviePoster =
+	"https://artgalleryofballarat.com.au/wp-content/uploads/2020/06/placeholder-image.png";
 
 const noResultsText = `
     <div class="message-404">
         <h2>No results found</h2>
         <p>Check your search and try again!</p>
     </div>
-`
+`;
 
-const singleMovieHtml = movie => `
+const singleMovieHtml = (movie) => `
     <div class="single-movie">
         <a href="https://google.com/search?q=${movie.Title}" target="_blank">
             <img class="movie-img"
-            src="${movie.Poster !== 'N/A' ? movie.Poster : defaultMoviePoster}"
+            src="${movie.Poster !== "N/A" ? movie.Poster : defaultMoviePoster}"
             alt="Poster ${movie.Title}"
             />
         </a>
@@ -29,43 +27,38 @@ const singleMovieHtml = movie => `
         <p class="movie-year">Year: ${movie.Year}</p>
         <p class="movie-category">Category: ${upperFirstLetter(movie.Type)}</p>
     </div>
-`
-const singleMovie = movieList => movieList.map( movie => singleMovieHtml(movie) )
+`;
+const singleMovie = (movieList) =>
+	movieList.map((movie) => singleMovieHtml(movie));
 
-const addMovieToTheList = (searchResults, movieList) => searchResults.map(movie => movieList.push(movie))
-
-
-
-
+const addMovieToTheList = (searchResults, movieList) =>
+	searchResults.map((movie) => movieList.push(movie));
 
 const fetchApi = async ({ search, printIn }) => {
+	const api = `http://cors.io/?https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`;
 
-    const api = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
+	const response = await fetch(api, {
+		method: "get",
+		Accept: "application/json",
+	});
+	const data = await response.json();
+	const searchResults = data.Search;
 
-    const response = await fetch(api, {
-        method: 'get',
-        Accept: 'application/json'
-    })
-    const data = await response.json()
-    const searchResults = data.Search
+	const movieList = [];
 
-    const movieList = []
+	if (searchResults != undefined) {
+		addMovieToTheList(searchResults, movieList);
+	}
 
-    if (searchResults != undefined) {
-        addMovieToTheList(searchResults, movieList)
-    }
+	if (movieList.length > 0) {
+		printIn.innerHTML = singleMovie(movieList);
+	} else {
+		printIn.innerHTML = noResultsText;
+	}
 
-    if (movieList.length > 0) {
-        printIn.innerHTML = singleMovie(movieList)
-    } else {
-        printIn.innerHTML = noResultsText
-    }
+	if (search == "") {
+		printIn.innerHTML = "";
+	}
+};
 
-
-    if (search == '') {
-        printIn.innerHTML = ''
-    }
-
-}
-
-export default fetchApi
+export default fetchApi;
